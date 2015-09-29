238448f5ce268d6299fbdde0d531a9dedf365e4b
loro102 <loro102@gmail.com>
2015-09-29 15:05:04 +0200

    inicial

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on September 28, 2015, 1:26 pm.
---
+++ src/Rumbo/AppBundle/RumboAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Rumbo\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RumboAppBundle extends Bundle
+{
+}
